60485612108e83398c6b11cff16ec49944935688
COLD <zhengjf65@163.com>
2025-11-30 23:39:47 +0800

    套餐分页查询业务代码开发及新增套餐代码修改

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface SetmealService {
 
@@ -9,5 +11,14 @@ public interface SetmealService {
       * 新增套餐
       * @param setMealDTO
       */
-    void save(SetmealDTO setMealDTO);
+    void saveWithDish(SetmealDTO setMealDTO);
+
+
+
+     /**
+      * 分页查询套餐
+      * @param setmealPageQueryDTO
+      * @return
+      */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,14 +1,20 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -28,7 +34,8 @@ public class SetmealServiceImpl implements SetmealService {
       * 新增套餐,同时需要保存套餐和菜品的关联关系
       * @param setMealDTO
       */
-    public void save(SetmealDTO setMealDTO) {
+     @Transactional
+    public void saveWithDish(SetmealDTO setMealDTO) {
         // 新增套餐
         Setmeal setMeal = new Setmeal();
         BeanUtils.copyProperties(setMealDTO,setMeal);
@@ -47,5 +54,22 @@ public class SetmealServiceImpl implements SetmealService {
 
         //新增套餐和菜品的关系
         setmealDishMapper.insertBatch(setmealdishes);
+    }
+
+
+
+
+     /**
+      * 套餐分页查询
+      * @param setmealPageQueryDTO
+      * @return
+      */
+    public PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        PageHelper.startPage(setmealPageQueryDTO.getPage(),setmealPageQueryDTO.getPageSize());
+
+
+        Page<SetmealVO> page = setmealMapper.pageQuery(setmealPageQueryDTO);
+
+        return new PageResult(page.getTotal(),page.getResult());
     }
 }
