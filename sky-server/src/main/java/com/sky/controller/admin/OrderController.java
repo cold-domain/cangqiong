c3f258559bbb32ff17549b4a4c449279294c7ab0
COLD <zhengjf65@163.com>
2025-12-06 18:50:43 +0800

    管理端查询订单详情业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -13,6 +13,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -52,5 +53,21 @@ public class OrderController {
         OrderStatisticsVO orderStatisticsVO = orderService.statistics();
 
         return Result.success(orderStatisticsVO);
+    }
+
+
+    /**
+     * 查询订单详情
+     * @param id
+     * @return
+     */
+    @GetMapping("/details/{id}")
+    @ApiOperation("查询订单详情")
+    public Result<OrderVO> details(@PathVariable Long id){
+        log.info("查询订单详情:{}", id);
+
+        OrderVO orderVO = orderService.details(id);
+
+        return Result.success(orderVO);
     }
 }
