03448b1f0622bdea40b2b11eb8e43939a8cd8dc5
COLD <zhengjf65@163.com>
2025-12-08 17:16:10 +0800

    营业额数据统计功能业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -0,0 +1,75 @@
+package com.sky.service.impl;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.apache.poi.util.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class ReportServiceImpl implements ReportService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+
+    /**
+     * 统计指定时间区间内的营业额数据
+     */
+    public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
+        //当前集合用于存放指定时间区间的所有日期
+        List<LocalDate> dateList = new ArrayList<>();
+
+        //添加日期
+        dateList.add(begin);
+        while(!begin.equals(end)){
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+
+        //查询营业额数据，遍历日期集合，查询数据库
+        List<Double> turnoverList = new ArrayList<>();
+        for (LocalDate date : dateList) {
+            //查询date日期的营业额，营业额：状态为“已完成”的订单金额合计
+            //此处date与order_time类型不同，需要设置好date的起始时间和结束时间
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+
+            //select sum(amount) from orders
+            // where order_time > beginTime and order_time < endTime and status = 5
+            //封装三个参数到map中
+            Map map = new HashMap();
+            map.put("begin", beginTime);
+            map.put("end", endTime);
+            map.put("status", Orders.COMPLETED);
+            Double turnover = orderMapper.sumByMap(map);
+
+            //如果查询结果为空，将营业额设置为0.0
+            turnover = turnover == null ? 0.0 : turnover;
+
+            turnoverList.add(turnover);
+        }
+
+
+        //封装结果，返回转换为字符串的日期列表和转换为字符串的营业额列表
+        return TurnoverReportVO
+                .builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .turnoverList(StringUtils.join(turnoverList,","))
+                .build();
+    }
+}
